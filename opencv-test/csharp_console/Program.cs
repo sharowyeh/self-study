4ed14b2431f8d299a5c91b72d724d1a3405e0837
sharow yeh <yuyu.yei@hotmail.com>
2022-11-11 16:12:54 +0800

    opencv-test: add .net core csharp console with SDL wrapper library, try to find a GUI for opencv features

---
+++ opencv-test/csharp_console/Program.cs
@@ -0,0 +1,58 @@
+﻿// project referenced nuget SDL2-CS wrapper package for language bindings:
+// https://github.com/flibitijibibo/SDL2-CS
+// https://www.libsdl.org/languages.php
+
+// Simple DirectMedia Layer is a cross-platform development library for
+//   low-level access HCI devices, OpenGL, Direct3D and etc GUI framework.
+//   For example, GUI of ffplay(ffmpeg)
+
+// this project should copy related sdl2 native dll files from nuget sdl2 redist packages
+//   located <user>/.nuget/packages/sdl2xxxx/<version>/build/native
+// tarent: it better download release binaries from github page, for the dependencies of these dependencies
+// The wrapper provides bindings for the following libraries:
+// - SDL2
+// - SDL2_gfx
+// - SDL2_image
+// - SDL2_mixer
+// - SDL2_ttf
+
+using SDL2;
+
+SDL.SDL_SetHint(SDL.SDL_HINT_WINDOWS_DISABLE_THREAD_NAMING, "1");
+
+SDL.SDL_Init(SDL.SDL_INIT_VIDEO);
+
+IntPtr window;
+IntPtr renderer;
+var code = SDL.SDL_CreateWindowAndRenderer(
+    300, 200, SDL.SDL_WindowFlags.SDL_WINDOW_RESIZABLE, out window, out renderer);
+
+IntPtr surface = SDL_image.IMG_Load("../Lenna_test_image.png");
+
+IntPtr texture = SDL.SDL_CreateTextureFromSurface(renderer, surface);
+
+SDL.SDL_FreeSurface(surface);
+
+SDL.SDL_Event evt;
+while (true)
+{
+    SDL.SDL_PollEvent(out evt);
+    if (evt.type == SDL.SDL_EventType.SDL_QUIT)
+        break;
+
+    SDL.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL.SDL_RenderClear(renderer);
+    SDL.SDL_RenderCopy(renderer, texture, IntPtr.Zero, IntPtr.Zero);
+    SDL.SDL_RenderPresent(renderer);
+}
+
+SDL.SDL_DestroyTexture(texture);
+SDL.SDL_DestroyRenderer(renderer);
+SDL.SDL_DestroyWindow(window);
+
+SDL.SDL_Quit();
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
