253fceff93793f61c911e3fe7262aada6ea67cfe
sharow yeh <yuyu.yei@hotmail.com>
2022-11-11 16:21:40 +0800

    wpf-test: add .net core projects for grpc service/app

---
+++ wpf-test/grpc-app/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Grpc.AspNetCore;
+
+namespace grpc_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
