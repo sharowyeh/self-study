cd26f34eaeadf396dcbe0fa54f08a37c378e1310
sharow yeh <yuyu.yei@hotmail.com>
2023-11-22 18:29:29 +0800

    debug for new feature in cvplot fork repo

--- opencv-test/win32-console/cvplot_debug.h
+++ opencv-test/win32-console/cvplot_debug.h
@@ -17,10 +17,18 @@
 
 //DEBUG: try to design 2d colormap, due to cv-plot strict linebase to drawing plot, change while drawing mechanism
 void show_plane_colormap() {
-	std::vector<double> v{.24f, .36f, .22f, .03f, .83f, .55f, .37f, .79f, .93f}; // try the 3x3
+	// try 8x6 size to draw color map
+	std::vector<double> v;
+	cv::Mat m(60, 80, CV_64FC1);
+	for (int i = 0; i < 60 * 80; i++) {
+		v.push_back(((i * 2) - 4800.f) / 4800.f);
+		m.at<double>(i / 80, i % 80) = ((i * 2) - 4800.f) / 4800.f;
+	}
 	// for linspec, use `p` represent plane chart
-	auto axes = CvPlot::plot(v, "pk");
-	cv::Mat mat = axes.render(600, 800);
+	auto axes = CvPlot::plane(m, "pk", "debug");
+	axes.setMargins(40, 20, 20, 40);
+	auto found = axes.find<CvPlot::Series>("debug");
+	cv::Mat mat = axes.render(600, 600);
 	cv::imshow("colormap", mat);
 	cv::waitKey();
 }
@@ -32,6 +40,8 @@ void show_line_plot() {
 	//  `o` means circle marker type, exntend x/+ for cross/plus symbol
 	//  color code only support b/g/r/c/y/m/k/w 8 types
 	auto axes = CvPlot::plot(v, "+k");
+	axes.xLabel("x");
+	// default axes margin x:80,30 y:40,45
 	//auto axes = CvPlot::plot(v, "-om");
 	cv::Mat mat = axes.render(600, 800);
 	cv::imshow("lineplot", mat);
@@ -52,6 +62,8 @@ void show_circle_plot() {
 	}
 	auto axes = CvPlot::makePlotAxes();
 	axes.create<CvPlot::Series>(xAxis, yAxis, "-g");
+	axes.create<CvPlot::Series>(xAxis, "-b");
+	auto ret = axes.find<CvPlot::Series>();
 	cv::Mat mat = axes.render(300, 400);
 	cv::imshow("circleplot", mat);
 	cv::waitKey();
